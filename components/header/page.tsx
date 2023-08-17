9db88612fd85f317de88f7c30a3a0fadfb658e79
SoRa <sonamraisx007@gmail.com>
2023-08-17 18:37:54 +0545

    guransh academy added to the header of the website on mobile view

--- components/header/page.tsx
+++ components/header/page.tsx
@@ -15,13 +15,19 @@ const Header = (props: Props) => {
     <>
       <NavModal openModal={openModal} setOpenModal={setOpenModal} />
       <div className="h-[10vh] sm:h-[12vh] w-full flex justify-between items-center px-6 md:px-16 relative bg-gray-100 shadow-md shadow-gray-500">
-        <Image
-          src="/logo2.jpg"
-          alt="Guransh Academy Logo"
-          width={100}
-          height={100}
-          className="h-10 sm:h-16 w-10 sm:w-16 rounded-full object-cover"
-        />
+        <div className="flex items-center">
+          <Image
+            src="/logo2.jpg"
+            alt="Guransh Academy Logo"
+            width={100}
+            height={100}
+            className="h-10 sm:h-16 w-10 sm:w-16 rounded-full object-cover"
+          />
+
+          <p className="sm:hidden flex ml-4 font-semibold text-lg">
+            Guransh Academy
+          </p>
+        </div>
 
         {/* Bar */}
         <Menu
